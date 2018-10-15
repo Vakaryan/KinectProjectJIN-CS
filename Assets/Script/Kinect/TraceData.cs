57ea095a55f62ef60fe827e73626afdb43621528
Léa <lea-saunier@hotmail.fr>
2018-10-15 20:21:27 +0200

    Adding project files

---
+++ Assets/Script/Kinect/TraceData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class TraceData : MonoBehaviour
+{
+    StreamWriter writer;
+
+    // Use this for initialization
+    void Start()
+    {
+        string fileName = "Trace.txt";
+        writer = new StreamWriter(fileName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<KinectPointController>().isTracked)
+        {
+            writer.WriteLine("x :" + transform.position.x + "; y :" + transform.position.y + "; z :" + transform.position.z + "; time : " + Time.deltaTime);
+        }
+    }
+
+
+    void OnDestroy()
+    {
+        writer.Close();
+    }
+}
---
+++ Assets/Script/Kinect/mvs.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mvs : MonoBehaviour {
+    public GameObject right_hand;
+    protected Vector3 real_pos;
+    protected Vector3 virtual_pos;
+    public int scale;
+
+	// Use this for initialization
+	void Start () {
+        real_pos = right_hand.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        real_pos = right_hand.transform.position;
+        virtual_pos = scale * real_pos;
+	}
+}
